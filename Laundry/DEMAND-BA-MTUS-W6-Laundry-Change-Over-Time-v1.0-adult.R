d81ac9bfa0686999784d3b26f9f6a4ab3c1490de
Ben Anderson <b.anderson@soton.ac.uk>
2014-10-24 12:57:55 +0100

    changed dataset for laundry R analysis
    switched to MTUS W6 & using stata import

--- Laundry/DEMAND-BA-MTUS-W6-Laundry-Change-Over-Time-v1.0-adult.R
+++ Laundry/DEMAND-BA-MTUS-W6-Laundry-Change-Over-Time-v1.0-adult.R
@@ -18,40 +18,53 @@
 rm(list = ls()) 
 
 # Loading the data
+# load as stata file
+library(foreign)
 # diary
-timeuse2005d <- read.csv("~/Documents/Work/Data/Social Science Datatsets/Time Use 2005/processed/timeusefinal_for_archive_diary_long_v2.0.csv")
+MTUSW6UK_d <- read.dta("~/Documents/Work/Data/Social Science Datatsets/MTUS/World 6/processed/MTUS-adult-episode-UK-only-wf.dta")
 # survey
-timeuse2005s <- read.csv("~/Documents/Work/Data/Social Science Datatsets/Time Use 2005/processed/timeusefinal_for_archive_survey_v2.0.csv")
+MTUSW6UK_s <- read.dta("~/Documents/Work/Data/Social Science Datatsets/MTUS/World 6/processed/MTUS-adult-aggregate-UK-only-wf.dta")
+# create a reduced survey frame with the few variables we need
+MTUSW6UK_s_redvars <- c("diarypid", "empstat", "urban")
+MTUSW6UK_s_red <- MTUSW6UK_s[MTUSW6UK_s_redvars]
 
 # merge
-timeuse2005all <- merge(timeuse2005d,timeuse2005s,by="serial")
+MTUSW6UK_m <- merge(MTUSW6UK_d,MTUSW6UK_s_red,by="diarypid")
 
 # Take a quick look at the data
-head(timeuse2005)
+head(MTUSW6UK_m)
 
-# attach this dataset so we don't have to keep specifying the table
-attach(timeuse2005)
+# Check what's in it?
+names(MTUSW6UK_m)
 
 # check the distribution of episodes by time of day and year of survey
-table("Half hour"= s_faketime)
+# NB: stata has already set the date to 1960! We need to re-format to half hours!
+table("Half hour"= MTUSW6UK_m$s_halfhour)
 
-# the data has no labels (intentionally)
-# 21 = laundry
+# 21 = "laundry, ironing, clothing repair"
+# We've imported from stata so we'll have to use the value label not the value
+# Could have imported as csv and then applied labels... might be easier
 
 # check incidence of laundry as primary & secondary act
-table("Laundry as primary"= pact == 21)
-table("Laundry as secondary"= sact == 21)
+table("Laundry as primary"= MTUSW6UK_m$main == "laundry, ironing, clothing repair")
+table("Laundry as secondary"= MTUSW6UK_m$sec == "laundry, ironing, clothing repair")
 
 # create 2 new variables (columns) which are 'laundry'
-timeuse2005$laundry_p[timeuse2005$pact == 21] <- 1 
-timeuse2005$laundry_s[timeuse2005$sact == 21] <- 1
-timeuse2005$laundry_all <- 0
-timeuse2005$laundry_all[timeuse2005$laundry_p == 1 | timeuse2005$laundry_p == 1] <- 1
+MTUSW6UK_m$laundry_p <- 0
+MTUSW6UK_m$laundry_p[MTUSW6UK_m$main == "laundry, ironing, clothing repair"] <- 1 
 
-table(timeuse2005$laundry_all)
+MTUSW6UK_m$laundry_s <- 0
+MTUSW6UK_m$laundry_s[MTUSW6UK_m$sec == "laundry, ironing, clothing repair"] <- 1 
+
+MTUSW6UK_m$laundry_all <- 0
+MTUSW6UK_m$laundry_all[MTUSW6UK_m$laundry_p == 1 | MTUSW6UK_m$laundry_s == 1] <- 1
+
+table(MTUSW6UK_m$laundry_all)
 
 # check location of laundry
 # lact = -1 (unknown), 1 = home, 2 = elsewhere
-table("Laundry as primary"= timeuse2005$pact == 21, timeuse2005$lact)
-table("Laundry as secondary"= timeuse2005$sact == 21, timeuse2005$lact)
+table("Laundry as primary"= MTUSW6UK_m$laundry_p == 1, MTUSW6UK_m$eloc)
+table("Laundry as secondary"= MTUSW6UK_m$laundry_s == 1, MTUSW6UK_m$eloc)
 
+laundry_all_tod <- table("All laundry" = MTUSW6UK_m$laundry_all == 1, MTUSW6UK_m$s_halfhour)
+plot(laundry_all_tod)
